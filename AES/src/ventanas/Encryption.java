3b51b1d31c5807cdcefffcd5e0f98571803acda7
josue ruiz <josue_zu@outlook.es>
2019-02-03 19:55:43 -0600

    AES

---
+++ AES/src/ventanas/Encryption.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ventanas;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import static org.apache.commons.codec.binary.Base64.decodeBase64;
+import static org.apache.commons.codec.binary.Base64.encodeBase64;
+/**
+ *
+ * @author josue
+ */
+public class Encryption { 
+    // Definición del tipo de algoritmo a utilizar (AES, DES, RSA)
+    private final static String alg = "AES";
+    // Definición del modo de cifrado a utilizar
+    private final static String cI = "AES/CBC/PKCS5Padding";
+ 
+    /**
+     * Función de tipo String que recibe una llave (key), un vector de inicialización (iv)
+     * y el texto que se desea cifrar
+     * @param key la llave en tipo String a utilizar
+     * @param iv el vector de inicialización a utilizar
+     * @param cleartext el texto sin cifrar a encriptar
+     * @return el texto cifrado en modo String
+     * @throws Exception puede devolver excepciones de los siguientes tipos: NoSuchAlgorithmException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException
+     */
+    public  String encrypt(String key, String iv, String cleartext) throws Exception {
+            Cipher cipher = Cipher.getInstance(cI);
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(), alg);
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes());
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivParameterSpec);
+            byte[] encrypted = cipher.doFinal(cleartext.getBytes());
+            return new String(encodeBase64(encrypted));
+    }
+ 
+    /**
+     * Función de tipo String que recibe una llave (key), un vector de inicialización (iv)
+     * y el texto que se desea descifrar
+     * @param key la llave en tipo String a utilizar
+     * @param iv el vector de inicialización a utilizar
+     * @param encrypted el texto cifrado en modo String
+     * @return el texto desencriptado en modo String
+     * @throws Exception puede devolver excepciones de los siguientes tipos: NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException
+     */
+    public  String decrypt(String key, String iv, String encrypted) throws Exception {
+            Cipher cipher = Cipher.getInstance(cI);
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(), alg);
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes());
+            byte[] enc = decodeBase64(encrypted);
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivParameterSpec);
+            byte[] decrypted = cipher.doFinal(enc);
+            return new String(decrypted);
+    }
+}
+ 
