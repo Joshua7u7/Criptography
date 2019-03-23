7a666553a73e29b475a07d0e31e577ba25d0e62d
josue ruiz <josue_zu@outlook.es>
2019-03-23 13:52:06 -0600

    All ´till services

---
+++ HASH/src/hash/Archivo.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hash;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ *
+ * @author josue
+ */
+public class Archivo {
+    
+    public static String rute(String actual_path,String title,String type){
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(title, type);
+        String rute;
+        File d = new File(actual_path);
+        try {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileFilter(filter);
+        chooser.setDialogTitle(title);
+        chooser.setCurrentDirectory(d);
+        int option = chooser.showOpenDialog(chooser);
+        if(option == JFileChooser.APPROVE_OPTION){
+            File f = chooser.getSelectedFile();
+            rute = f.getAbsolutePath();
+            return rute;
+            }
+        }
+        catch(Exception e) {
+            JOptionPane.showMessageDialog(null,"You didnt select any text yet");
+        }
+        return null;
+    }
+    
+    public static void mostrarArchivo(String ruta)
+    {
+         try{ 
+         //definiendo la ruta en la propiedad file
+         Runtime.getRuntime().exec("cmd /c start "+ruta);
+        }catch(IOException e){
+        } 
+    }
+    
+    public static void escribirArchivo(String file,String rute) throws FileNotFoundException, UnsupportedEncodingException
+    {
+        File nuevo=new File(rute);        
+        try (PrintWriter fw = new PrintWriter(nuevo,"utf-8")) {
+            fw.println(file);
+            fw.close();
+        }
+    }
+    
+    public static String leerArchivo(String file) throws FileNotFoundException, IOException
+    {
+        StringBuilder cad = new StringBuilder();
+        String cadena;
+        //FileReader f = new FileReader(rute);
+        File f = new File(file);
+        try (BufferedReader b = new BufferedReader(new InputStreamReader(new FileInputStream(f),StandardCharsets.UTF_8))) {
+             while((cadena = b.readLine())!=null) {
+                 cad.append(cadena).append("\n");
+             }}
+        cad.deleteCharAt(cad.length()-1);
+        return cad.toString();   
+    }
+    
+}
---
+++ HASH/src/hash/Hash.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hash;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author josue
+ */
+public class Hash {
+    
+    public static String getSha(String input)
+    {
+        try { 
+  
+            // Static getInstance method is called with hashing SHA 
+            MessageDigest md = MessageDigest.getInstance("SHA-256"); 
+  
+            // digest() method called 
+            // to calculate message digest of an input 
+            // and return array of byte 
+            byte[] messageDigest = md.digest(input.getBytes()); 
+  
+            // Convert byte array into signum representation 
+            BigInteger no = new BigInteger(1, messageDigest); 
+  
+            // Convert message digest into hex value 
+            String hashtext = no.toString(16); 
+  
+            while (hashtext.length() < 32) { 
+                hashtext = "0" + hashtext; 
+            } 
+  
+            return hashtext; 
+        } 
+        // For specifying wrong message digest algorithms 
+        catch (NoSuchAlgorithmException e) { 
+            System.out.println("Exception thrown"
+                               + " for incorrect algorithm: " + e); 
+            return null; 
+        } 
+    }
+    
+    public static boolean verifySha(String rute_old) throws IOException
+    {
+        String output_text = Archivo.leerArchivo(rute_old);
+        
+        char[] array = output_text.toCharArray();
+        
+        String aux="";
+        String oo ="";
+        
+        for(int i=0;i<output_text.length()-65;i++)
+            oo+=array[i];
+        
+        for(int i=output_text.length()-64;i<output_text.length();i++)
+            aux+=array[i];
+        
+        String newSha = getSha(oo);
+        
+        System.out.println(aux);
+        System.out.println(oo.length());
+        
+        return (aux.equalsIgnoreCase(newSha));
+    }
+    
+    
+    
+}
